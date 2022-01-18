ba472862907d1a2e747b573ff0775293e4055342
AnthonyAlgo <43523456+AnthonyAlgo@users.noreply.github.com>
2022-01-18 04:03:45 +0000

    Branch-12-Day-13 Worked on data transformation with pandas map and apply

--- DataTransformation.py
+++ DataTransformation.py
@@ -14,5 +14,13 @@ print(data_frame['cars'])
 
 for x in data_frame.index:
   if data_frame.loc[x, 'rating'] < 5:
-    data_frame.loc[x] = 5, 'Honda'
+    data_frame.loc[x] = ['Honda',5]
 print(data_frame)
+
+print(data_frame.loc[1, 'cars'])
+
+def test_map(x: int):
+  if x > 1:
+    return 1
+transformed_df = data_frame['rating'].map(test_map)
+print(transformed_df)
