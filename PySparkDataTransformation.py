bd8742d3f98877d2f8723b60376e3ac193b372dc
AnthonyAlgo <43523456+AnthonyAlgo@users.noreply.github.com>
2022-01-20 06:04:32 +0000

    Branch-14

--- PySparkDataTransformation.py
+++ PySparkDataTransformation.py
@@ -1,5 +1,13 @@
-from pyspark.sql import SparkSession
+
+from pyspark.sql import *
+from pyspark.sql.functions import *
 spark = SparkSession.builder.appName("Pyspark Experiments").getOrCreate()
 df = spark.createDataFrame([(1, 1.0), (2, 2.0)], ["int", "float"])
 print(df.show())
+
+def test_transformation(x):
+  return x+100
 df.filter('int > 1').show()
+df.select(transform('int',test_transformation)).show()
+
+
